72626f6f930192a20fd5adcbea4067b1ddee83ba
Edinamm Cita Amekuedee <edinamamekuedee4@gmail.com>
2025-07-16 19:58:32 +0000

    Initial commit with files created and basic structure

---
+++ index.js
@@ -0,0 +1,10 @@
+
+let myLeads = [];
+const inputBtn = document.getElementById("input-btn")
+const   inputEl = document.getElementById("input-el")
+
+
+inputBtn.addEventListener("click", function(){
+     myLeads.push("www.awesome.com")
+});
+
