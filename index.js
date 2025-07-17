4d3209eebebad02d5d34c0e1d17b840745bcbc6a
Edinamm Cita Amekuedee <edinamamekuedee4@gmail.com>
2025-07-17 20:53:57 +0000

    Pulled HTML elements in JS and learnt a new way of appending from JS into HTML

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 
-let myLeads = [];
-const inputBtn = document.getElementById("input-btn")
-const   inputEl = document.getElementById("input-el")
+let myLeads = ["Html","Css","Js"];
+const inputBtn = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el")
 
 
 inputBtn.addEventListener("click", function(){
-     myLeads.push("www.awesome.com")
+     myLeads.push(inputEl.value)
+     console.log(myLeads)
 });
+
+for (let i = 0; i < myLeads.length; i++){
+     ulEl.innerHTML += "<li>" + myLeads[i] + "<li/>";
+     /*Alternative way of doing this
+     /create element 
+     set text content
+     append to ul/
+     const = document.createElement("li")
+     li.textContent = myLeads[i]
+     ulEl.append(li)*/
+
 
