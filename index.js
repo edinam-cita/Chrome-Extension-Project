cad565a5f32487c7e84b7818978de0fa8605311c
Edinamm Cita Amekuedee <edinamamekuedee4@gmail.com>
2025-07-21 18:08:46 +0000

    Added a Save tab button,learnt to use Google API,refactored a section of the code and learnt the difference btn parameters and arguments

--- index.js
+++ index.js
@@ -3,23 +3,32 @@ const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
+const leadsFromStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.getElementById("tab-btn");
 
 //localStorage only support strings
-const leadsFromStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromStorage) {
   myLeads = leadsFromStorage;
-  renderLeads();
+  render(myLeads);
 }
 
-function renderLeads() {
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
+
+function render(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     //listItems += "<li><a target='_blank'href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>";
     listItems += `
     <li>
-    <a target="_blank" href="${myLeads[i]}"> 
-    ${myLeads[i]} 
+    <a target="_blank" href="${leads[i]}"> 
+    ${leads[i]} 
     </a>
     </li>
     `;
@@ -27,18 +36,17 @@ function renderLeads() {
   ulEl.innerHTML = listItems;
 }
 
-
 deleteBtn.addEventListener("dblclick", function () {
   localStorage.clear();
   myLeads = [];
-  renderLeads();
+  render(myLeads);
 });
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads();
+  render(myLeads);
   console.log(localStorage.getItem("myLeads"));
 });
 
